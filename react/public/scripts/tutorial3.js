6fff51beae53060cef17ade40882dac2be5d4aa9
Tim Siwula <tcsiwula@usfca.edu>
2016-06-14 07:30:13 -0700

    created background video html page

---
+++ react/public/scripts/tutorial3.js
@@ -0,0 +1,106 @@
+// tutorial3.js
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+// tutorial4.js
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+// tutorial5.js
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+            </div>
+        );
+    }
+});
+
+// tutorial6.js
+var Comment = React.createClass({
+    render: function() {
+        var md = new Remarkable();
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {md.render(this.props.children.toString())}
+            </div>
+        );
+    }
+});
+
+// tutorial7.js
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+
+// tutorial8.js
+var data = [
+    {id: 1, author: "Pete Hunt", text: "This is one comment"},
+    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+// tutorial9.js
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox data={data} />,
+    document.getElementById('content')
+);
+
+
+
+
+
+
+
+
